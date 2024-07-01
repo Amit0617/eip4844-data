f4f162d4aa2b5ce007c7546be7c29bfb53b8e2f9
Amit0617 <71893015+Amit0617@users.noreply.github.com>
2024-07-01 12:01:50 +0530

    Plot charts on monthly basis

--- scripts/filter-private-transactions.py
+++ scripts/filter-private-transactions.py
@@ -36,20 +36,32 @@ df = pd.read_csv(f'{directory}/private-transactions2.csv')
 transactions_per_day = df['detect_date'].value_counts().sort_index()
 # unique dates
 dates = transactions_per_day.index
+# divide dates into separate months array
+months = []
+for date in dates:
+    if date.split('-')[1] not in months:
+        months.append(date.split('-')[1])
+
 print("Writing to README.md")
 with open(f'../README.md', 'w') as readme:
     # number of transactions per day in the README.md for creating mermaid xy chart
-    readme.write('```mermaid\n')
-    # configs for the chart
-    readme.write('---\n')
-    readme.write('config:\n')
-    readme.write('    xyChart:\n')
-    readme.write('        width: ' + str(len(dates) * 80) + '\n')     # width of the chart should be dates length * 80 for no overlapping
-    readme.write('        height: 600\n')
-    readme.write('---\n')
-    # chart data
-    readme.write('xychart-beta title "Number of private blob transactions per day"\n')
-    readme.write('x-axis "Date" [' + ','.join([str(date) for date in dates]) + ']' + '\n')
-    readme.write('y-axis "Number of Transactions"\n')
-    readme.write('bar[' + ','.join([str(tx_count) for tx_count in transactions_per_day]) + ']' + '\n')
-    readme.write('```')
+    for month in months:
+        # print(f"Month: {month}")
+        month_dates = [date for date in dates if date.split('-')[1] == month]
+        # print(month_dates)
+        readme.write('```mermaid\n')
+        # configs for the chart
+        readme.write('---\n')
+        readme.write('config:\n')
+        readme.write('    xyChart:\n')
+        readme.write('        width: ' + str(len(month_dates) * 80) + '\n')     # width of the chart should be dates length * 80 for no overlapping
+        readme.write('        height: 600\n')
+        readme.write('---\n')
+        # chart data
+        readme.write(f'xychart-beta title "Number of private blob transactions per day in {month}th month"\n')
+        readme.write('x-axis "Date" [' + ','.join([str(date) for date in month_dates]) + ']' + '\n')
+        readme.write('y-axis "Number of Transactions"\n')
+        # split the transactions per day into separate arrays for each month
+        transactions_per_day = [transactions_per_day[date] for date in month_dates]
+        readme.write('bar[' + ','.join([str(tx_count) for tx_count in transactions_per_day]) + ']' + '\n')
+        readme.write('```')
